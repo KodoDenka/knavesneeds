8f33f22e124142a4ae1a8dbad276cb56006b4e15
KodoDenka <46654157+KodoDenka@users.noreply.github.com>
2025-12-29 20:54:44 -0800

    Fabric Datagen work

--- fabric/src/main/java/dev/manasnow/knavesneeds/KnavesFabricDatagen.java
+++ fabric/src/main/java/dev/manasnow/knavesneeds/KnavesFabricDatagen.java
@@ -1,30 +1,13 @@
 package dev.manasnow.knavesneeds;
 
-
 import dev.manasnow.knavesneeds.datagen.CommonRecipeGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.minecraft.data.recipes.FinishedRecipe;
-
-import java.util.function.Consumer;
+import net.minecraft.data.DataGenerator;
 
 public class KnavesFabricDatagen implements DataGeneratorEntrypoint {
     @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-        pack.addProvider(MyFabricRecipeProvider::new);
-    }
-
-    private static class MyFabricRecipeProvider {
-        private MyFabricRecipeProvider(FabricDataGenerator output) {
-            super(output);
-        }
-
-        @Override
-        public void generate(Consumer<FinishedRecipe> exporter) {
-
-            CommonRecipeGenerator.generate(exporter);
-
-        }
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        //CommonRecipeGenerator.generate(generator);
     }
 }
